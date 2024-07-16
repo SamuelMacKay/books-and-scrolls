9da64e43197eb4b71e55a41b1227996bb21f8908
Samuel Mackay <samuel.john.mackay@gmail.com>
2024-07-16 10:00:41 +0000

    added modal initialisation

--- static/js/script.js
+++ static/js/script.js
@@ -18,6 +18,7 @@ $(document).ready(function () {
   $(".sidenav").sidenav({edge: "right"});
   $(".collapsible").collapsible();
   $("select").formSelect();
+  $('.modal').modal();
   
   validateMaterializeSelect();
   function validateMaterializeSelect() {
